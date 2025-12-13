4f11a77d3d2f3b1977391f1d1bfb8abd7abb88e6
rasyidcode <rasyidcode7@gmail.com>
2025-12-13 07:51:16 +0700

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,3 +26,28 @@ vim.o.shiftwidth = 4
 -- Enable break indent
 vim.o.breakindent = true
 
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insentive searching UNLESS \C or one more capital letters in search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
